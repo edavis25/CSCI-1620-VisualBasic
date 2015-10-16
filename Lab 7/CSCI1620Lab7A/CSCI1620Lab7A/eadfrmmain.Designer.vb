716203849da7c64d1a29b5da19994957613cce9d
Eric Davis <davis.25811@gmail.com>
2015-10-16 15:33:16 -0400

    Finished most of Lab 7A. Needs looked over for completeness and thoroughly tested

---
+++ Lab 7/CSCI1620Lab7A/CSCI1620Lab7A/eadfrmmain.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class eadfrmmain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(eadfrmmain))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintSelectedFlavorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintSelectedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PreviewSelectedToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintAllFlavorsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PreviewAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddCoffeeFlavorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveCoffeeFlavorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearCoffeeListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DisplayCoffeeCountToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.SyrupListBox = New System.Windows.Forms.ListBox()
+        Me.CoffeeComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PrintAllPrintDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PrintSelectedPrintDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(345, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintSelectedFlavorsToolStripMenuItem, Me.PrintAllFlavorsToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PrintSelectedFlavorsToolStripMenuItem
+        '
+        Me.PrintSelectedFlavorsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintSelectedToolStripMenuItem, Me.PreviewSelectedToolStripMenuItem})
+        Me.PrintSelectedFlavorsToolStripMenuItem.Name = "PrintSelectedFlavorsToolStripMenuItem"
+        Me.PrintSelectedFlavorsToolStripMenuItem.Size = New System.Drawing.Size(186, 22)
+        Me.PrintSelectedFlavorsToolStripMenuItem.Text = "Print &Selected Flavors"
+        '
+        'PrintSelectedToolStripMenuItem
+        '
+        Me.PrintSelectedToolStripMenuItem.Name = "PrintSelectedToolStripMenuItem"
+        Me.PrintSelectedToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.PrintSelectedToolStripMenuItem.Text = "&Print Selected"
+        '
+        'PreviewSelectedToolStripMenuItem
+        '
+        Me.PreviewSelectedToolStripMenuItem.Name = "PreviewSelectedToolStripMenuItem"
+        Me.PreviewSelectedToolStripMenuItem.Size = New System.Drawing.Size(162, 22)
+        Me.PreviewSelectedToolStripMenuItem.Text = "Pre&view Selected"
+        '
+        'PrintAllFlavorsToolStripMenuItem
+        '
+        Me.PrintAllFlavorsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintAllToolStripMenuItem, Me.PreviewAllToolStripMenuItem})
+        Me.PrintAllFlavorsToolStripMenuItem.Name = "PrintAllFlavorsToolStripMenuItem"
+        Me.PrintAllFlavorsToolStripMenuItem.Size = New System.Drawing.Size(186, 22)
+        Me.PrintAllFlavorsToolStripMenuItem.Text = "Print &All Flavors"
+        '
+        'PrintAllToolStripMenuItem
+        '
+        Me.PrintAllToolStripMenuItem.Name = "PrintAllToolStripMenuItem"
+        Me.PrintAllToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PrintAllToolStripMenuItem.Text = "&Print All"
+        '
+        'PreviewAllToolStripMenuItem
+        '
+        Me.PreviewAllToolStripMenuItem.Name = "PreviewAllToolStripMenuItem"
+        Me.PreviewAllToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PreviewAllToolStripMenuItem.Text = "Pre&view All"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(183, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(186, 22)
+        Me.ExitToolStripMenuItem.Text = "E&xit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddCoffeeFlavorToolStripMenuItem, Me.RemoveCoffeeFlavorToolStripMenuItem, Me.ClearCoffeeListToolStripMenuItem, Me.DisplayCoffeeCountToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'AddCoffeeFlavorToolStripMenuItem
+        '
+        Me.AddCoffeeFlavorToolStripMenuItem.Name = "AddCoffeeFlavorToolStripMenuItem"
+        Me.AddCoffeeFlavorToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.AddCoffeeFlavorToolStripMenuItem.Text = "&Add Coffee Flavor"
+        '
+        'RemoveCoffeeFlavorToolStripMenuItem
+        '
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Name = "RemoveCoffeeFlavorToolStripMenuItem"
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.RemoveCoffeeFlavorToolStripMenuItem.Text = "&Remove Coffee Flavor"
+        '
+        'ClearCoffeeListToolStripMenuItem
+        '
+        Me.ClearCoffeeListToolStripMenuItem.Name = "ClearCoffeeListToolStripMenuItem"
+        Me.ClearCoffeeListToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.ClearCoffeeListToolStripMenuItem.Text = "&Clear Coffee List"
+        '
+        'DisplayCoffeeCountToolStripMenuItem
+        '
+        Me.DisplayCoffeeCountToolStripMenuItem.Name = "DisplayCoffeeCountToolStripMenuItem"
+        Me.DisplayCoffeeCountToolStripMenuItem.Size = New System.Drawing.Size(190, 22)
+        Me.DisplayCoffeeCountToolStripMenuItem.Text = "&Display Coffee Count"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "&About"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.SyrupListBox)
+        Me.GroupBox1.Controls.Add(Me.CoffeeComboBox)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 39)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(322, 132)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Select Coffee and Syrup"
+        '
+        'SyrupListBox
+        '
+        Me.SyrupListBox.FormattingEnabled = True
+        Me.SyrupListBox.Items.AddRange(New Object() {"(None)", "Chocolate", "Hazelnut", "Irish Cream", "Orange"})
+        Me.SyrupListBox.Location = New System.Drawing.Point(215, 49)
+        Me.SyrupListBox.Name = "SyrupListBox"
+        Me.SyrupListBox.Size = New System.Drawing.Size(86, 69)
+        Me.SyrupListBox.Sorted = True
+        Me.SyrupListBox.TabIndex = 3
+        '
+        'CoffeeComboBox
+        '
+        Me.CoffeeComboBox.FormattingEnabled = True
+        Me.CoffeeComboBox.Items.AddRange(New Object() {"Chocolate Almond", "Espresso Roast", "Jamaica Blue Mountain", "Kona Blend", "Vanilla Nut"})
+        Me.CoffeeComboBox.Location = New System.Drawing.Point(30, 52)
+        Me.CoffeeComboBox.Name = "CoffeeComboBox"
+        Me.CoffeeComboBox.Size = New System.Drawing.Size(145, 21)
+        Me.CoffeeComboBox.Sorted = True
+        Me.CoffeeComboBox.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(212, 33)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(66, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Syrup Flavor"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(25, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Coffee Flavor"
+        '
+        'PrintAllPrintDocument
+        '
+        '
+        'PrintSelectedPrintDocument
+        '
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'eadfrmmain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(345, 184)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "eadfrmmain"
+        Me.Text = "R 'n R for Reading 'n Refreshment"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintSelectedFlavorsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintSelectedToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PreviewSelectedToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintAllFlavorsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintAllToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PreviewAllToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AddCoffeeFlavorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RemoveCoffeeFlavorToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearCoffeeListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DisplayCoffeeCountToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents SyrupListBox As System.Windows.Forms.ListBox
+    Friend WithEvents CoffeeComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PrintAllPrintDocument As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintSelectedPrintDocument As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog1 As System.Windows.Forms.PrintPreviewDialog
+
+End Class
---
+++ Lab 7/CSCI1620Lab7A/CSCI1620Lab7A/eadfrmmain.vb
@@ -0,0 +1,201 @@
+﻿Public Class eadfrmmain
+
+    ' -----CODE FOR "EDIT" MENU ITEM ----->
+    Private Sub AddCoffeeFlavorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AddCoffeeFlavorToolStripMenuItem.Click
+        ' Action for the "Add Coffee Flavor" menu item. This item takes user entry in the drop down combo box 
+        ' and adds the user entry to the list.
+
+        With CoffeeComboBox
+
+            ' Test to make sure user input is not blank.
+            If .Text <> "" Then
+
+                ' Check for Duplicate user entry into the Coffee Flavor dropdown box with a loop.
+                Dim ItemFoundBoolean As Boolean
+                Dim ItemIndexInteger As Integer
+
+                Do Until (ItemFoundBoolean = True) Or (ItemIndexInteger = .Items.Count)
+                    If .Text.ToUpper = .Items(ItemIndexInteger).ToString().ToUpper Then
+                        ItemFoundBoolean = True
+                        Exit Do
+                    Else
+                        ItemIndexInteger += 1
+                    End If
+                Loop
+
+                ' IF test to handle a duplicate entry. ELSE if no duplicate is found, add item to list.
+                If ItemFoundBoolean = True Then
+                    MessageBox.Show("Duplicate Item.", "Add Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Else
+                    .Items.Add(.Text)   '<-- Long form = CoffeeComboBox.Items.Add(CoffeeComboBox.Text)
+                    .Text = ""          '<-- Clears out the CoffeeComboBox
+                End If
+
+            Else
+                ' Else for the IF checking the CoffeeFlavor for blank entry.
+                MessageBox.Show("Please enter a coffee flavor to add", "Missing Data", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End If
+
+            ' Focus given back to CoffeeComboBox regardless.
+            .Focus()
+
+        End With
+    End Sub
+
+    Private Sub RemoveCoffeeFlavorToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RemoveCoffeeFlavorToolStripMenuItem.Click
+        ' "Remove Coffee Flavor" event. This code will check to make sure the default -1 is not selected (meaning nothing selected) and display
+        ' an error message if there is nothing selected (if the index is -1, nothing is selected). After checking the index it will remove
+        ' whatever coffee flavor index has been selected.
+
+        With CoffeeComboBox
+            If .SelectedIndex <> -1 Then
+                .Items.RemoveAt(.SelectedIndex)
+            Else
+                MessageBox.Show("Please select a coffee flavor to remove", "No Coffee Flavor Selected", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                .Focus()
+            End If
+        End With
+    End Sub
+
+    Private Sub DisplayCoffeeCountToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles DisplayCoffeeCountToolStripMenuItem.Click
+        ' "Display Coffee Count" event. Create a variable to hold all the string message and then display a count of how many items 
+        ' are currently in the Coffee Combo Box.
+
+        Dim MessageString As String
+
+        MessageString = "The number of coffee types is: " & CoffeeComboBox.Items.Count
+
+        MessageBox.Show(MessageString, "R 'n R Coffee Type Count", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub ClearCoffeeListToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ClearCoffeeListToolStripMenuItem.Click
+        ' "Clear Coffee List" event will clear all of the items inside of the Coffee Flavor ComboBox. Ask the user beforehand if they are
+        ' sure they want to clear list and then store that result into a variable to check with an IF test.
+
+        Dim ClearAllDialogResult As DialogResult
+
+        ClearAllDialogResult = MessageBox.Show("Are you sure you want to clear all the flavors from the list?", "Clear Coffee List?", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2)
+
+        If ClearAllDialogResult = DialogResult.Yes Then
+            CoffeeComboBox.Items.Clear()
+        End If
+    End Sub
+
+
+
+    ' -----CODE FOR "HELP" MENU ITEM ----->
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        ' Displays the generic About Form with info from the assembly information.
+
+        AboutBox1.ShowDialog()
+    End Sub
+
+
+
+    ' -----CODE FOR PRINT SELECTED ITEMS ----->
+    Private Sub PreviewSelectedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PreviewSelectedToolStripMenuItem.Click
+        ' "Print Selected Flavors Preview" event. When the user clicks the preview, assign the correct print document to PrintPreviewDialog1
+        ' and then simply show the dialog. IF test both user inputs to make sure there is data selected.
+
+        ' Check syrup listbox. If nothing selected (-1) automatically select "(None)" option with index = 0
+        If SyrupListBox.SelectedIndex = -1 Then
+            SyrupListBox.SelectedIndex = 0
+        End If
+
+        ' Check Coffee Flavor combobox. If something is selected (NOT equals -1) then print PREVIEW. ELSE show error message.
+        If CoffeeComboBox.SelectedIndex <> -1 Then
+            PrintPreviewDialog1.Document = PrintSelectedPrintDocument
+            PrintPreviewDialog1.ShowDialog()
+        Else
+            MessageBox.Show("Please select a coffee flavor.", "No Coffee Selected", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+
+    End Sub
+
+    Private Sub PrintSelectedToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintSelectedToolStripMenuItem.Click
+        ' "Print Selected" event. Controls the print directly click. IF test both users inputs to make sure data is selected.
+
+        ' Check syrup listbox. If nothing selected (-1) automatically select "(None)" option with index = 0
+        If SyrupListBox.SelectedIndex = -1 Then
+            SyrupListBox.SelectedIndex = 0
+        End If
+
+        ' Check Coffee Flavor combobox. IF something is selected (NOT EQUALS -1) then PRINT. ELSE show error message.
+        If CoffeeComboBox.SelectedIndex <> -1 Then
+            PrintSelectedPrintDocument.Print()
+        Else
+            MessageBox.Show("Please select a coffee flavor.", "No Coffee Selected", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+        End If
+
+    End Sub
+
+    Private Sub PrintSelectedPrintDocument_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintSelectedPrintDocument.PrintPage
+        ' Sets up the "Print Selected" print document for use in both print and print preview. This code formats the page for the printer.
+
+        ' Variables to set font, set location, and create line spacing.
+        Dim PrintFont As New Font("Arial", 12)
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim LineHeightSingle As Single = PrintFont.GetHeight + 2    '<---Use this variable to increment Y coordinate for each new line. Gets height & adds 2 pixels.
+        Dim HorizontalPrintLocationSingle As Single = e.MarginBounds.Left
+        Dim VerticalPrintLocationSingle As Single = e.MarginBounds.Top
+        Dim PrintLineString As String        '<--- Assign what needs printed to this variable.
+
+        ' EX: e.Graphics.DrawString(*DATA TO DISPLAY*, *FONT*, *COLOR*, *X LOCATION*, *Y LOCATION*)   <---Ignore * and replace with variables.
+
+        ' Setup and display the HEADER. Set variable to display and then send it, X&Y location, font, and font color. (Not in that order)
+        PrintLineString = "Print Selected Item"         '<---Set data
+        e.Graphics.DrawString(PrintLineString, HeadingFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)   '<---Display Data
+
+        PrintLineString = "By Eric A Davis"
+        VerticalPrintLocationSingle += LineHeightSingle     '<---Increment the Y coordinate to setup for the next line on page
+        e.Graphics.DrawString(PrintLineString, HeadingFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)
+
+        ' Setup and display the BODY. Display whatever is selected in the CoffeeComboBox and SyrupBox
+        VerticalPrintLocationSingle += LineHeightSingle * 2     '<--- Multiply by 2 to create a space between header/body
+        PrintLineString = "Coffee: " & CoffeeComboBox.Text & "     Syrup: " & SyrupListBox.Text
+        e.Graphics.DrawString(PrintLineString, PrintFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)
+    End Sub
+
+
+
+    ' ----- CODE FOR PRINT ALL ITEMS ----->
+    Private Sub PreviewAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PreviewAllToolStripMenuItem.Click
+        ' "Print All Flavors Preview" event. Assign Print All Document to Print Preview Dialog and call.
+
+        PrintPreviewDialog1.Document = PrintAllPrintDocument
+        PrintPreviewDialog1.ShowDialog()
+    End Sub
+
+    Private Sub PrintAllToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles PrintAllToolStripMenuItem.Click
+        ' "Print All" event. Prints all of the flavors without showing print preview.
+
+        PrintAllPrintDocument.Print()
+    End Sub
+
+    Private Sub PrintAllPrintDocument_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintAllPrintDocument.PrintPage
+        ' Sets up the "Print All" print document for use in both print and print preview. This code formats the page for the printer.
+        ' Uses a loop to cycle through all the items in the lists.
+
+        ' Variables to set font, set location, and create line spacing.
+        Dim PrintFont As New Font("Arial", 12)
+        Dim HeadingFont As New Font("Arial", 14, FontStyle.Bold)
+        Dim LineHeightSingle As Single = PrintFont.GetHeight + 2    '<---Use this variable to increment Y coordinate for each new line. Gets height & adds 2 pixels.
+        Dim HorizontalPrintLocationSingle As Single = e.MarginBounds.Left
+        Dim VerticalPrintLocationSingle As Single = e.MarginBounds.Top
+        Dim PrintLineString As String        '<--- Assign what needs printed to this variable.
+
+        ' EX: e.Graphics.DrawString(*DATA TO DISPLAY*, *FONT*, *COLOR*, *X LOCATION*, *Y LOCATION*)   <---Ignore * and replace with variables.
+
+        ' Setup and display the HEADER.
+        e.Graphics.DrawString("Flavors", HeadingFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)
+
+        ' Loop to setup and display contents of Coffee Flavor combobox. The loop will execute equal to the number of items in the combobox
+        ' by using: .Items.Count - 1 (subtract one because index starts at 0). Each execution of the loop will format the Y line position,
+        ' set index position to the string variable, display, and then increment ListIndexInteger
+        For ListIndexInteger As Integer = 0 To CoffeeComboBox.Items.Count - 1
+            VerticalPrintLocationSingle += LineHeightSingle
+            PrintLineString = CoffeeComboBox.Items(ListIndexInteger).ToString()
+            e.Graphics.DrawString(PrintLineString, PrintFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)
+        Next ListIndexInteger
+    End Sub
+End Class
